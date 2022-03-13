7d4cd6c9e130f909c6b4c60e328284021764b689
birdybro <aberusugi@gmail.com>
2022-03-13 17:20:23 -0600

    fix error

--- mycore.sv
+++ mycore.sv
@@ -275,7 +275,7 @@ TopLevel TopLevel
 );
 
 assign CLK_VIDEO = clk_sys;
-assign CE_PIXEL = ce_pix;
+assign CE_PIXEL = 1;
 
 reg  [26:0] act_cnt;
 always @(posedge clk_sys) act_cnt <= act_cnt + 1'd1; 
